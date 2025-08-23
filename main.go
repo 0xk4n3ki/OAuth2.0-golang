91063569327f91147fe228b0b229ffe798e87e1e
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-08-24 05:11:14 +0530

    modified the structure

--- main.go
+++ main.go
@@ -1,19 +1,1 @@
 package main
-
-import (
-	"github.com/0xk4n3ki/OAuth-2.0-golang/config"
-	"github.com/0xk4n3ki/OAuth-2.0-golang/controllers"
-
-	"github.com/gofiber/fiber/v2"
-)
-
-func main() {
-	app := fiber.New()
-
-	config.GoogleConfig()
-
-	app.Get("/google_login", controllers.GoogleLogin)
-	app.Get("/google_callback", controllers.GoogleCallback)
-
-	app.Listen(":9000")
-}
