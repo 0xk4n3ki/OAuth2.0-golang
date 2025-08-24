a3b9a6a722bf9b4f48bce322142e632ff02c55fd
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-08-24 05:48:37 +0530

    started the project

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	
+}
